42d5024f5a4c2c14585e1fe6f43eddeeb7d94cdf
Robin La Rondelle <rarondel@avans.nl>
2018-05-15 17:07:36 +0200

    Added asserts to authentication.js

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,65 +1,93 @@
-
- //Created by dkroeske on 25/04/2018.
+//Created by dkroeske on 25/04/2018.
 const settings = require('../config.json');
 const moment = require('moment');
 const jwt = require('jwt-simple');
+const error = require('../classes/error');
+const assert = require('assert');
 
+module.exports = {
 
-// Encode (van username naar token)
-function encodeToken(userID, userEmail) {
+    // Encode (van username naar token)
+    encodeToken: function (userID, userEmail) {
 
-    //Payload maken
-    const payload = {
+        //Asserts
+        try {
 
-        //een "houdsbaarheidsdatum" toevoegen aan de code
-        exp: moment().add(10, 'days').unix(),
-        iat: moment().unix(),
-        sub: userID,
-        email: userEmail
-    };
+            //assert email
+            assert(typeof (userEmail) === "string", "userID must be a string.");
 
-    //Maak een encoded waarde aan
-    const encode = jwt.encode(payload, settings.secretkey);
+            //assert userID
+            assert(typeof (userID) === "string", "userID must be a string.");
+            assert(isNaN(userID) === false, "userID must be a number");
+            assert(userId.indexOf('-') === -1, "userID can\'t be negative");
+            assert(userId.indexOf('.') === -1, "userID can\'t be a decimal");
 
-    //Log de waarde
-    console.log(encode);
+        } catch (e) {
 
-    //Geef de waarde terug
-    return encode;
-}
+            //Geef een nieuwe error omdat een assert is mislukt
+            const ApiError = new error(e.toString(), 422);
+            next(ApiError);
+            return;
 
-
-
-// Decode (van token naar username)
-function decodeToken(token, callback) {
-
-    try {
-        
-        //verkrijg de payload
-        const payload = jwt.decode(token, settings.secretkey);
-
-        //Haal de tijdwaarde op en vergelijk dit
-        const now = moment().unix();
-
-        //Check of de huidige tijdwaarde groter is dan de  tijdwaarde van de payload
-        if (now > payload.exp) {
-            console.log("Token has expired");
         }
 
-        // Return
-        callback(null, payload);
+        //Payload maken
+        const payload = {
 
-    } catch(error) {
+            //een "houdsbaarheidsdatum" toevoegen aan de code
+            exp: moment().add(10, 'days').unix(),
+            iat: moment().unix(),
+            sub: userID,
+            email: userEmail
+        };
 
-        callback(error, null);        
+        //Maak een encoded waarde aan
+        const encode = jwt.encode(payload, settings.secretkey);
+        console.log(encode);
+
+        //Geef de waarde terug
+        return encode;
+    },
+
+
+    // Decode (van token naar username)
+    decodeToken: function (token, callback) {
+
+        try{
+
+            //token assert
+            assert(typeof (token) === "string", "Token must be a string");
+            assert(token.includes('') === false, "Token may not be empty");
+
+        } catch(e)  {
+
+            //Error handler
+            const ApiError = new error(e.toString(), 422);
+            next(ApiError);
+            return;
+            
+        }
+
+        try {
+
+            //verkrijg de payload
+            const payload = jwt.decode(token, settings.secretkey);
+
+            //Haal de tijdwaarde op en vergelijk dit
+            const now = moment().unix();
+
+            //Check of de huidige tijdwaarde groter is dan de  tijdwaarde van de payload
+            if (now > payload.exp) {
+                console.log("Token has expired");
+            }
+
+            // Return
+            callback(null, payload);
+
+        } catch (error) {
+
+            callback(error, null);
+        }
+
     }
-
 }
-
-//Exports
-module.exports = {
-    encodeToken,
-    decodeToken
-};
-
-
